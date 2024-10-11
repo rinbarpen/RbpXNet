78d0fd863ac40eb9c96aa3355369da42bd20f03e
LpoutYoumu <LpoutYoumu@gmail.com>
2024-10-11 14:50:51 +0800

    trash files

--- models/models.py
+++ models/models.py
@@ -1,22 +1,22 @@
 def select_model(model: str, *args, **kwargs):
     match model:
         case 'UNet':
-            from models.samples.unet import UNet
+            from .samples.unet import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case 'RASW-UNet':
-            from models.like.unet.RASW import UNet
+            from .like.unet.RASW import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case 'SWA-UNet':
-            from models.like.unet.SWA import UNet
+            from .like.unet.SWA import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case 'ML-UNet':
-            from models.like.unet.MultiLink import UNet
+            from .like.unet.MultiLink import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case 'Which-Way-UNet':
-            from models.like.unet.WhichWayImportant import UNet
+            from .like.unet.WhichWayImportant import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case 'WayAttention-UNet':
-            from models.like.unet.WayAttention import UNet
+            from .like.unet.WayAttention import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case _:
             raise ValueError(f'Not supported model: {model}')
