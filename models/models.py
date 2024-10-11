cd00d9245a66a97c99bf3621c207090ad60ff669
LpoutYoumu <LpoutYoumu@gmail.com>
2024-10-11 14:46:10 +0800

    fix a error

--- models/models.py
+++ models/models.py
@@ -2,7 +2,7 @@ def select_model(model: str, *args, **kwargs):
     match model:
         case 'UNet':
             from models.samples.unet import UNet
-            return UNet(kwargs['n_channels'], kwargs['n_classes'], kwargs['use_bilinear'])
+            return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
         case 'RASW-UNet':
             from models.like.unet.RASW import UNet
             return UNet(kwargs['n_channels'], kwargs['n_classes'], False)
--- scripts/train.py
+++ scripts/train.py
@@ -2,7 +2,7 @@ import subprocess
 
 cmds = dict()
 
-cmds['UNet'] = r'python main.py --project Segment-Drive ' \
+cmds['UNet'] = r'python main.py --project Segment-Drive --author LpoutYoumu' \
     '-m UNet --n_channels 1 --n_classes 1 ' \
     '--classes vein ' \
     '-lr 3e-5 -e 200 -b 1 ' \
@@ -13,7 +13,7 @@ cmds['UNet'] = r'python main.py --project Segment-Drive ' \
     '--train ' \
     '--save_every_n_epoch 40'.split()
 
-cmds['Which-Way-UNet'] = r'python main.py --project Segment-Drive ' \
+cmds['Which-Way-UNet'] = r'python main.py --project Segment-Drive --author LpoutYoumu' \
     '-m Which-Way-UNet --n_channels 1 --n_classes 1 ' \
     '--classes vein ' \
     '-lr 3e-5 -e 200 -b 1 ' \
@@ -24,7 +24,7 @@ cmds['Which-Way-UNet'] = r'python main.py --project Segment-Drive ' \
     '--train ' \
     '--save_every_n_epoch 40'.split()
 
-cmds['WayAttention-UNet'] = r'python main.py --project Segment-Drive ' \
+cmds['WayAttention-UNet'] = r'python main.py --project Segment-Drive --author LpoutYoumu' \
     '-m WayAttention-UNet --n_channels 1 --n_classes 1 ' \
     '--classes vein ' \
     '-lr 3e-5 -e 200 -b 1 ' \
@@ -35,7 +35,7 @@ cmds['WayAttention-UNet'] = r'python main.py --project Segment-Drive ' \
     '--train ' \
     '--save_every_n_epoch 40'.split()
 
-cmds['SWA-UNet'] = r'python main.py --project Segment-Drive ' \
+cmds['SWA-UNet'] = r'python main.py --project Segment-Drive --author LpoutYoumu' \
     '-m SWA-UNet --n_channels 1 --n_classes 1 ' \
     '--classes vein ' \
     '-lr 3e-5 -e 200 -b 1 ' \
